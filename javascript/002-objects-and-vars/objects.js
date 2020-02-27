c08e45f93000189ab82abdba89fc9c74ea527620
dongero <dagostino.zach@gmail.com>
2020-02-27 07:58:23 -0800

    added javascript

---
+++ javascript/002-objects-and-vars/objects.js
@@ -0,0 +1,29 @@
+
+
+var time = "3:43";
+
+var rectangle = {
+
+	x:0,
+	y:10,
+	width:100,
+	height:200,
+
+	print:function(what_to_say) {
+
+		console.log(this.width + ", " + what_to_say);
+
+
+	}
+
+};
+
+rectangle.print("I'm a rectangle!")
+
+function sayHello() {
+
+	alert("hello!");
+
+}
+
+sayHello();
